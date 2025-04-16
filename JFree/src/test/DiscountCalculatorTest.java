5e5be878441d937d0b53bf4ccc2a3069af4eb765
LodgyMagdy <lodgymagdy1@gmail.com>
2025-04-16 12:50:06 +0200

    FinalVersionOfCalculatorAndManager

--- JFree/src/test/DiscountCalculatorTest.java
+++ JFree/src/test/DiscountCalculatorTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import JFree.DiscountCalculator;
+import JFree.IDiscountCalculator;
 import org.jfree.data.time.Week;
 import org.junit.Test;
 
@@ -8,23 +9,122 @@ import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+
 public class DiscountCalculatorTest {
 
     @Test
     public void testIsTheSpecialWeekWhenFalse() throws Exception {
-        // Arrange
+
+        //Arrange
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2025, Calendar.MARCH, 22);  // March 22, 2025
+        calendar.set(2025, Calendar.MARCH, 22);
         Date date = calendar.getTime();
         Week week = new Week(date);
+        IDiscountCalculator calculator = new DiscountCalculator(week);
 
-        // Act
+        //Act
+        boolean actual = calculator.isTheSpecialWeek();
 
-
-        // Assert
-
+        //Assert
+        assertFalse(actual);
     }
 
-    // Test missing cases ( JUNE, 23 is a date in week 26 )
+    @Test
+    public void testIsTheSpecialWeekWhenTrue() throws Exception {
+
+        //Arrange
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2025, Calendar.JUNE, 23);
+        Date date = calendar.getTime();
+        Week week = new Week(date);
+        IDiscountCalculator calculator = new DiscountCalculator(week);
+
+        // Act
+        boolean actual = calculator.isTheSpecialWeek();
+
+        // Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testGetDiscountPercentageWhenEven() throws Exception {
+
+        //Arrange
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2025, Calendar.JANUARY, 10);
+        Date date = calendar.getTime();
+        Week week = new Week(date);
+        IDiscountCalculator calculator = new DiscountCalculator(week);
+        int expected = 7 ;
+
+        //Act
+        int actual = calculator.getDiscountPercentage();
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    //it's a bug
+    @Test
+    public void testGetDiscountPercentageWhenOdd() throws Exception {
+
+        //Arrange
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2025, Calendar.OCTOBER, 3);
+        Date date = calendar.getTime();
+        Week week = new Week(date);
+        IDiscountCalculator calculator = new DiscountCalculator(week);
+        int expected = 5 ;
+
+        //Act
+        int actual = calculator.getDiscountPercentage();
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    //it's a bug
+    @Test
+    public void testGetDiscountPercentageWithInvalidDate() throws Exception {
+
+       //Assert + Act
+        assertThrows(IllegalArgumentException.class, () -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(2025, Calendar.FEBRUARY, 30);
+            Date date = calendar.getTime();
+            Week week = new Week(date);
+            IDiscountCalculator calculator = new DiscountCalculator(week);
+            calculator.getDiscountPercentage();
+        });
+    }
+
+    @Test
+    public void testGetDiscountPercentageWhenWeekIsNull() throws Exception {
+
+        //Arrange
+        Week nullWeek = null;
+
+        //Assert + Act
+        assertThrows(NullPointerException.class, () -> {
+            DiscountCalculator calculator = new DiscountCalculator(nullWeek);
+            calculator.getDiscountPercentage();
+        });
+    }
+
+    @Test
+    public void testGetDiscountPercentageWhenWeekIsZero() {
+
+        //Assert + Act
+        assertThrows(IllegalArgumentException.class, () -> {
+            Week week = new Week(0, 2025);
+            DiscountCalculator calculator = new DiscountCalculator(week);
+            calculator.getDiscountPercentage();
+        });
+    }
+
+
+
+
 
 }
--- JFree/src/test/DiscountManagerTest.java
+++ JFree/src/test/DiscountManagerTest.java
@@ -10,6 +10,7 @@ public class DiscountManagerTest {
 
     @Test
     public void testCalculatePriceWhenDiscountsSeasonIsFalse() throws Exception {
+
         // Arrange
         boolean isDiscountsSeason = false;
         double originalPrice = 100.0;
@@ -17,25 +18,169 @@ public class DiscountManagerTest {
 
         Mockery mockingContext = new Mockery();
         IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
-        mockingContext.checking(new Expectations(){
-            {
-                // make sure that none of the functions are called
-            }
-        });
+
+        mockingContext.checking(new Expectations() {{
+            never(mockedDependency);
+        }});
+
         DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
         // Act
-
-
+        double actualPrice = discountManager.calculatePriceAfterDiscount(originalPrice);
 
         // Assert
-        // make sure that mocking Expectations Is Satisfied
-        // make sure that the actual value exactly equals the expected value
+        mockingContext.assertIsSatisfied();
+        assertEquals(expectedPrice, actualPrice, 0.001);
     }
 
     @Test
     public void testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsTrue() throws Exception {
 
+        // Arrange
+        boolean isDiscountsSeason = true;
+        double originalPrice = 100.0;
+        double expectedPrice = 80.0;
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        mockingContext.checking(new Expectations() {{
+            oneOf(mockedDependency).isTheSpecialWeek(); will(returnValue(true));
+        }});
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act
+        double actualPrice = discountManager.calculatePriceAfterDiscount(originalPrice);
+
+        // Assert
+        mockingContext.assertIsSatisfied();
+        assertEquals(expectedPrice, actualPrice, 0.001);
     }
 
-    // test missing cases
+    //it's a bug
+    @Test
+    public void testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsFalse() throws Exception {
+
+        // Arrange
+        boolean isDiscountsSeason = true;
+        double originalPrice = 100.0;
+        int discountPercentage = 5;
+        double expectedPrice = originalPrice * discountPercentage / 100.0;
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        mockingContext.checking(new Expectations() {{
+            oneOf(mockedDependency).isTheSpecialWeek(); will(returnValue(false));
+            oneOf(mockedDependency).getDiscountPercentage(); will(returnValue(discountPercentage));
+        }});
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act
+        double actualPrice = discountManager.calculatePriceAfterDiscount(originalPrice);
+
+        // Assert
+        mockingContext.assertIsSatisfied();
+        assertEquals(expectedPrice, actualPrice, 0.001);
+    }
+
+    @Test
+    public void testCalculatePriceWhenPriceIsZero() throws Exception {
+
+        // Arrange
+        boolean isDiscountsSeason = true;
+        double price = 0.0;
+        double expectedPrice = 0.0;
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        mockingContext.checking(new Expectations() {{
+            oneOf(mockedDependency).isTheSpecialWeek(); will(returnValue(true));
+        }});
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act
+        double actualPrice = discountManager.calculatePriceAfterDiscount(price);
+
+        // Assert
+        mockingContext.assertIsSatisfied();
+        assertEquals(expectedPrice, actualPrice, 0.001);
+    }
+
+    @Test
+    public void testCalculatePriceWhenPriceIsNull() {
+
+        // Arrange
+        boolean isDiscountsSeason = true;
+        Double price = null;
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Assert + Act
+        assertThrows(NullPointerException.class, () -> {
+            discountManager.calculatePriceAfterDiscount(price);
+        });
+    }
+
+    @Test
+    public void testCalculatePriceWhenPriceIsInt() throws Exception {
+
+        // Arrange
+        boolean isDiscountsSeason = true;
+        int intPrice = 100;
+        double expectedPrice = 80.0;
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        mockingContext.checking(new Expectations() {{
+            oneOf(mockedDependency).isTheSpecialWeek(); will(returnValue(true));
+        }});
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act
+        double actualPrice = discountManager.calculatePriceAfterDiscount(intPrice);
+
+        // Assert
+        mockingContext.assertIsSatisfied();
+        assertEquals(expectedPrice, actualPrice, 0.001);
+    }
+
+    @Test
+    public void testCalculatePriceWhenPriceIsString() throws Exception {
+
+        // Arrange
+        boolean isDiscountsSeason = true;
+        String priceStr = "not-a-number";
+
+        Mockery mockingContext = new Mockery();
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act + Assert
+        assertThrows(NumberFormatException.class, () -> {
+            double parsedPrice = Double.parseDouble(priceStr);
+            discountManager.calculatePriceAfterDiscount(parsedPrice);
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
